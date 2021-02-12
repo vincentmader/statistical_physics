9db7ae85a01d3a97a40fa86cf88c3d5a662f9622
Vincent C. Mader <vincent.mader@icloud.com>
2021-02-12 18:18:04 +0100

    continued on task 4

--- 11/code/main.py
+++ 11/code/main.py
@@ -6,10 +6,11 @@ import numpy as np
 from tqdm import tqdm
 
 
+random.seed(7)
+
 J = 1
 k_B = 1
 N = 32
-temperatures = [0.5, 1.5, 2.5, 3]
 
 
 def initialize_spins(N):
@@ -22,39 +23,30 @@ def initialize_spins(N):
     return grid
 
 
-def apply_periodic_bounds(cell_idx, N):
-
-    if cell_idx >= N:
-        cell_idx -= N
-    elif cell_idx < 0:
-        cell_idx += N
-
-    return cell_idx
-
-
 def get_flip_energy(grid, i, j):
 
-    H = 0
-    for di in [-1, 1]:
-        for dj in [-1, 1]:
-            H += - J * grid[
-                apply_periodic_bounds(i + di, N)
-            ][
-                apply_periodic_bounds(j + dj, N)
-            ]
+    def apply_periodic_bounds(cell_idx, N):
+        if cell_idx >= N:
+            cell_idx -= N
+        elif cell_idx < 0:
+            cell_idx += N
+        return cell_idx
 
-    return H
+    dE = 0  # calculate energy difference after flip
+    for di in [-1, 0, 1]:  # loop over neighbors
+        for dj in [-1, 0,  1]:
+            if di == dj == 0:
+                continue  # no self-interaction
+            # get row/col index for neighbor
+            i_neighbor = apply_periodic_bounds(i+di, N)
+            j_neighbor = apply_periodic_bounds(j+dj, N)
+            # get spin of neighbor
+            s_neighbor = grid[i_neighbor][j_neighbor]
+            # subtract current state's energy, add new state's energy
+            dE -= -J * s_neighbor * grid[i][j]
+            dE += -J * s_neighbor * (-grid[i][j])
 
-
-def get_magnetization(grid):
-    N = grid.shape[0]
-
-    magnetization = 0
-    for i in range(N):
-        for j in range(N):
-            magnetization += grid[i][j]
-
-    return magnetization / N**2
+    return dE
 
 
 def flip_random_spin(grid, T):
@@ -63,23 +55,34 @@ def flip_random_spin(grid, T):
         beta = 1 / (k_B * T)
         return np.exp(-beta * dE)
 
-    i = random.choice(range(N))
-    j = random.choice(range(N))
-    flip = False
-
+    # choose random grid cell
+    i, j = random.choice(range(N)), random.choice(range(N))
     dE = get_flip_energy(grid, i, j)
+
+    flip = False
     if dE < 0:  # flip spin
         flip = True
-    if dE > 0:  # TODO: >= 0?
+    if dE >= 0:  # flip only with Boltzmann probability
         if random.uniform(0, 1) < boltzmann_prob(dE, T):
             flip = True
 
     if flip:
-        grid[i][j] = not grid[i][j]
+        grid[i][j] *= -1
     return grid
 
 
-def plot_magnetizations(magnetizations):
+def get_magnetization(grid):
+    N = grid.shape[0]
+
+    magnetization = 0
+    for i in range(N):  # sum over all spins
+        for j in range(N):
+            magnetization += grid[i][j]
+
+    return magnetization / N**2
+
+
+def plot_magnetizations(magnetizations, temperatures):
 
     def get_color_gradient(from_color, to_color, nr_of_samples):
         gradient = Color(from_color).range_to(Color(to_color), nr_of_samples)
@@ -95,6 +98,7 @@ def plot_magnetizations(magnetizations):
             color=colors[idx],
         )
 
+    plt.xlabel('nr. of spin flips / $N^2$')
     plt.ylabel(r'magnetization $\langle M\rangle$')
     plt.legend(loc='lower right')
     plt.savefig('../figures/magnetization_vs_time.pdf')
@@ -102,17 +106,20 @@ def plot_magnetizations(magnetizations):
 
 def main():
 
+    temperatures = [1.5, 3]
+
     magnetizations = []
     for T in temperatures:
         grid = initialize_spins(N)
+
         magnetization = []
-        for run_idx in tqdm(range(60)):
+        for run_idx in tqdm(range(300)):
             for _ in range(N**2):
                 grid = flip_random_spin(grid, T)
             magnetization.append(get_magnetization(grid))
         magnetizations.append(magnetization)
 
-    plot_magnetizations(magnetizations)
+    plot_magnetizations(magnetizations, temperatures)
 
 
 if __name__ == "__main__":
